2752ad961b19956546f14a25fa04905e8b6ef5dd
Reagan Cline <reaganecline@gmail.com>
2016-11-14 21:14:37 +0000

    add comments + revise mysql script

--- questions.sh
+++ questions.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Ask for c9 username to use 
 echo -e "What is your c9 username?"
 read USERNAME
 echo -e "\e[31mWhat is your name?"
@@ -12,14 +14,17 @@ read SONG
 echo -e "\e[93mWhat is your favorite book?"
 read -e BOOK
 
+# Generate date stamp and unique id number
 DATE=$(date +"%F_%T")
 uuid=$(cat /proc/sys/kernel/random/uuid)
 
+# Send answers to CSV file
 echo -e "\e[30m$uuid,$DATE,$NAME,$AGE,$MOVIE,$SONG,$BOOK" >> answers.csv
 
+# Use assignment4_questions database
+mysql "USE assignment4_questions";
 
-mysql USE 
-
-mysql -u $USERNAME -p INSERT INTO 'assignment4_questions' 
-    ('UUID','Date','Name','Age','Movie','Song','Book')
-    VALUES ($uuid, $DATE, $NAME, $AGE, $MOVIE, $SONG, $BOOK);
+# Send answers into mySQL database
+mysql -u $USERNAME -p 
+    "INSERT INTO assignment4_answers (UUID,Date,Name,Age,Movie,Song,Book)
+    VALUES ($uuid, $DATE, $NAME, $AGE, $MOVIE, $SONG, $BOOK);"
