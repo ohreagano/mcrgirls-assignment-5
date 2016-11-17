75d1b7bed79da96444f003d8c4029c63dfb6492c
Reagan Cline <reaganecline@gmail.com>
2016-11-17 00:52:27 +0000

    added comments to script

--- answers.sql
+++ answers.sql
@@ -39,7 +39,7 @@ CREATE TABLE `table5` (
 
 LOCK TABLES `table5` WRITE;
 /*!40000 ALTER TABLE `table5` DISABLE KEYS */;
-INSERT INTO `table5` VALUES ('8713830b-00ee-4f4b-8a59-e94c58a7b4ef','2016-11-16_21:52:17','Celine Yuwono','19','Mammamia','Mammamia','The Odyssey'),('7c822a33-d984-41e8-8b27-6d74ffdcc881','2016-11-16_21:53:31','Bob Smith','22','MZX','Soierjfto','Bukk'),('ab2040ef-a638-4d81-88b9-81397592fd31','2016-11-16_22:05:45','Reagan','21','Bluemoon','Drops of Jupiter','The Fault in our Stars'),('6050fd33-cac4-4a77-b03b-0341db41b7a9','2016-11-16_22:07:29','Jaylin','35','Up','Broccoli','The Great Gatsby'),('03228db6-0a19-4a82-9cdd-ac4ae2f7e761','2016-11-16_22:12:34','Christiana','28','Love Actually','23','Movie Time'),('8d2eb18c-7cc8-4ad6-9ba3-8006ca174ad8','2016-11-16_22:13:46','Julia','29','Crossroads','Clarity','The Hound and the Fox');
+INSERT INTO `table5` VALUES ('8713830b-00ee-4f4b-8a59-e94c58a7b4ef','2016-11-16_21:52:17','Celine Yuwono','19','Mammamia','Mammamia','The Odyssey'),('7c822a33-d984-41e8-8b27-6d74ffdcc881','2016-11-16_21:53:31','Bob Smith','22','MZX','Soierjfto','Bukk'),('ab2040ef-a638-4d81-88b9-81397592fd31','2016-11-16_22:05:45','Reagan','21','Bluemoon','Drops of Jupiter','The Fault in our Stars'),('6050fd33-cac4-4a77-b03b-0341db41b7a9','2016-11-16_22:07:29','Jaylin','35','Up','Broccoli','The Great Gatsby'),('03228db6-0a19-4a82-9cdd-ac4ae2f7e761','2016-11-16_22:12:34','Christiana','28','Love Actually','23','Movie Time'),('8d2eb18c-7cc8-4ad6-9ba3-8006ca174ad8','2016-11-16_22:13:46','Julia','29','Crossroads','Clarity','The Hound and the Fox'),('4104ee3a-b53f-4a72-b063-c4f46964d006','2016-11-17_00:45:33','Reagan','21','American Beauty','The Battle of New Orleans','Ratners Star'),('43e829bb-d5ed-4f5c-a537-9b7d72cf4599','2016-11-17_00:47:40','Ronald','45','The Dark Knight','Summertime','East of Eden'),('15bd7602-9479-49c0-9dc0-d8171184ec6b','2016-11-17_00:51:25','Dan','23','Moneyball','Pedal Point Blues','Picture of Dorian Grey');
 /*!40000 ALTER TABLE `table5` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-16 22:13:46
+-- Dump completed on 2016-11-17  0:51:25
--- questions.sh
+++ questions.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-
-# Ask for c9 username to use 
+# Put c9 username in a variable to use in mysql command lines
 echo -e "What is your c9 username?"
 read USERNAME
 
-# import the dump file
+# Import dump from .SQL file
+echo -e "Now importing dump in answers.sql to database5..."
 mysql --user="$USERNAME" --password database5 < answers.sql
 
+# Ask questions and assign user answers to variables
 echo -e "\e[31mWhat is your name?"
 read NAME
 echo -e "\e[32mWhat is your age?"
@@ -19,19 +20,20 @@ read SONG
 echo -e "\e[93mWhat is your favorite book?"
 read -e BOOK
 
+# Generate date stamp and unique ID for each pass through script
+# Assign each to a variable
 DATE=$(date +"%F_%T")
 uuid=$(cat /proc/sys/kernel/random/uuid)
 
-echo -e "\e[30m$uuid,$DATE,$NAME,$AGE,$MOVIE,$SONG,$BOOK" >> answers.csv
+echo -e "\e[0mNow inserting your answers into database5..."
 
-# Enter mysql command line - Put all mysql commands inside "EOF" tags
-
+# Enter mysql command line and execute command that enters row of data into table 
 mysql --user="$USERNAME" --password="" --database="database5" --execute="
 INSERT INTO table5 (UUID, Date, Name, Age, Movie, Song, Book)
 VALUES ('$uuid', '$DATE', '$NAME', '$AGE', '$MOVIE', '$SONG', '$BOOK');"
 
-# end of mysql command line
-
-# output the dump
-
+echo -e "Now dumping database5 into answers.sql..."
+# Output dump into .SQL file
 mysqldump --user="$USERNAME" --password="" database5 > answers.sql
+
+echo -e "Done."
